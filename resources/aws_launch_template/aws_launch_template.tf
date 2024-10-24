c010da194841948c6ae50bee5fd6534835e3e856
Bolatov Alau <bolatovalau@gmail.com>
2024-10-24 01:05:44 -0500

    last no

--- resources/aws_launch_template/aws_launch_template.tf
+++ resources/aws_launch_template/aws_launch_template.tf
@@ -14,22 +14,10 @@ resource "aws_launch_template" "aws_insance_template" {
 #!/bin/bash
 sudo apt update
 sudo apt install -y apache2
-echo "RewriteEngine On
-RewriteRule ^reviews$ /reviews.html [L]" | sudo tee /var/www/html/.htaccess
 cd /var/www
 sudo chmod 777 html
 cd html
 echo "<html><h1>${var.autoscaling_group_name}_index.html</h1></html>" > index.html
-echo "<html><h1>${var.autoscaling_group_name}_reviews.html</h1></html>" > reviews.html
-echo "<html><h1>${var.autoscaling_group_name}_reviews-api.html</h1></html>" > reviews-api.html
-sudo systemctl restart apache2
-sudo a2enmod rewrite
-echo "<Directory /var/www/html>
-    AllowOverride All
-</Directory>" | sudo tee /etc/apache2/conf-available/rewrite.conf
-sudo a2enconf rewrite
-echo "RewriteEngine On RewriteRule ^reviews-api$ /reviews-api.html [L] RewriteRule ^reviews$ /reviews.html [L]" | sudo tee /var/www/html/.htaccess
-sudo systemctl restart apache2
 EOF
   )
 
